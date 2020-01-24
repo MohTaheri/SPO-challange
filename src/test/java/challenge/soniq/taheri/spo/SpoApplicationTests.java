c24d32e610ccaf5558bbb5e08a24295e40d2abb3
Mohammad Taheri <m.taherice@gmail.com>
2020-01-24 11:31:55 +0330

    add first test for check invalid workforce request

--- src/test/java/challenge/soniq/taheri/spo/SpoApplicationTests.java
+++ src/test/java/challenge/soniq/taheri/spo/SpoApplicationTests.java
@@ -1,13 +1,56 @@
 package challenge.soniq.taheri.spo;
 
-import org.junit.jupiter.api.Test;
+import challenge.soniq.taheri.spo.controllers.model.WorkforceInfoRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.validation.Validator;
 
-@SpringBootTest
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // for restTemplate
+//@ActiveProfiles("test")
 class SpoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private static final ObjectMapper om = new ObjectMapper();
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    /*
+    {
+        "timestamp":"2019-03-05T09:34:13.207+0000",
+        "status":400,
+        "errors":["Author is not allowed."]
+    }
+ */
+    @Test
+    public void invalidWorkforceInfoRequest() throws JSONException {
+
+        String workforceInfoRequest = "{\"rooms\":[ 23, 23 ,12], \"senior\":12,\"junior\":2}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(workforceInfoRequest, headers);
+
+        //Try exchange
+        ResponseEntity<String> response = restTemplate.exchange("/api/spi", HttpMethod.POST, entity, String.class);
+
+        String expectedJson = "{\"status\":400,\"errors\":[\"Workforce Info is not valid.\"]}";
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        JSONAssert.assertEquals(expectedJson, response.getBody(), false);
+
+    }
 
 }
