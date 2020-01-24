03bfebe17cdef3ebebe76acd5963268203b843c2
Mohammad Taheri <m.taherice@gmail.com>
2020-01-25 00:37:49 +0330

    fix typo

--- src/main/java/challenge/soniq/taheri/spo/controllers/SPOController.java
+++ src/main/java/challenge/soniq/taheri/spo/controllers/SPOController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 @RequestMapping({"/api/spo"})
 public class SPOController {
 
-    private OptimiseStaffs optimiseStaffs = null;
+    private OptimiseStaffs optimiseStaffs;
 
     public SPOController(OptimiseStaffs optimiseStaffs) {
         this.optimiseStaffs = optimiseStaffs;
